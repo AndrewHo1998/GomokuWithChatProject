84c477672359fb0c4cc1e27a484da715663f5773
zhangxin96 <1291540075@qq.com>
2018-06-09 16:44:23 +0800

    Add files via upload

--- server.java
+++ server.java
@@ -1,169 +1,101 @@
 package server;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-import java.io.*;
-import java.net.*;
-
-public class server
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.thread;
+ 
+class myServerThread implements Runnable
 {
-    public static void main(String[] args)
+    private ServerSocket serv;
+    private Socket client=null;
+    public int ServerN=0;
+    String clientName;
+    
+    public myServerThread (int listN) throws Exception
     {
-        // TODO Auto-generated method stub
-        server demoQQ_Server = new server();
+    	this.ServerN=listN;
+    	serv=new ServerSocket(8000+listN);
     }
-    public server()
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//serv.bind("127.0.0.1");
+		try
+		{
+		this.client=serv.accept();
+		//此步阻塞线程等待客户端连接
+		InputStream is=client.getInputStream();
+		OutputStream os=client.getOutputStream();
+		
+		
+		}catch (IOException ioe)
+		{
+			System.out.println(ioe.getMessage());
+		}
+	}
+	
+    char[] ConectionEstablished(String clientName, String clientID)
     {
-        
-        //初始化界面,开始界面的多线程
-        MyPanel mp;        
-        mp = new MyPanel();
-        
-        Thread t1 = new Thread(mp);
-        t1.start();
-        
-        //服务器getter
-        MyServer mServer = new MyServer();
-        Thread t2 = new Thread(mServer);
-        t2.start();
-        
-        //服务器sender
-        MyServer_writer myClient_writer = new MyServer_writer();
-        Thread t3 = new Thread(myClient_writer);
-        t3.start();
-        
-    }
-}
-
-//共享参数类
-class MyBaseServer
-{
-    //定义参数，务必让发送信息和接收信息的参数共享
-    static Socket s;
-    static ServerSocket ss;
-    static String send_ino,get_ino;
-}
-
-class MyServer_writer extends MyBaseServer implements Runnable //QQ写者
-{
-
-    @Override
-    public void run()
-    {
-        // TODO Auto-generated method stub
-        while(true)
-        {
-            try
-            {
-                Thread.sleep(500);
-            }
-            catch (InterruptedException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            sendIno();
-            send_ino = null;
-            //get_ino = null;
-        }
-    }
-    //发送信息
-        public void sendIno()
-        {
-            if(send_ino == null)
-            {
-                return;
-            }
-            try
-            {
-                //发送数据
-                PrintWriter pw = new PrintWriter(s.getOutputStream(),true);
-                //获取发送内容
-                pw.println(send_ino);
-                send_ino = "Client :" + send_ino +"\n";
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-}
-
-class MyServer extends MyBaseServer implements Runnable
-{        
-    public MyServer()
-    {
-        //下面是主要功能
-        try
-        {
-            get_ino = "welcome to chat!\n";
-            ss = new ServerSocket(9999);
-            s = ss.accept();
-        }
-        catch (IOException e)
-        {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-        }        
-    }
-
-    //传递返回信息
-    public void getIno()
-    {
-        //要拥有一个判断去跳出这个读取，否则一旦进入try块，如果没有信息进来，读者会一直占用cpu而不会进入到写者，除非写者也开多线程
-        /*
-        if(get_ino == null)
-        {
-            return;
-        }
-        */
-        try
-        {
-            //读数据
-            InputStreamReader inr = new InputStreamReader(s.getInputStream());
-            BufferedReader bf = new BufferedReader(inr);
-            get_ino = "Opponent :"+bf.readLine()+"\n";
-        }
-        catch (IOException e)
-        {
-            return;
-        }
+    	char[] Buffer=new char[50];
+    	Buffer[0]='E';
+    	clientName.getChars(1, clientName.length(), Buffer, 1);
+    	clientID.getChars(0, clientID.length(), Buffer, 30);
+    	return Buffer;
     }
     
-    @Override
-    public void run()
+    char[] Disconnection(String clientName, String clientID)
     {
-        // TODO Auto-generated method stub
-        while(true)
-        {
-            try
-            {
-                Thread.sleep(500);
-            }
-            catch (InterruptedException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            getIno();
-            //sendIno();
-           // send_ino = null;
-            //get_ino = null;
-        }
+    	char[] Buffer=new char[50];
+    	Buffer[0]='D';
+    	clientName.getChars(1, clientName.length(), Buffer, 1);
+    	clientID.getChars(0, clientID.length(), Buffer, 30);
+    	return Buffer;
     }
+    
+    char[] SendMessage(String toWhom, String context)
+    {
+    	char[] Buffer=new char[200];
+    	Buffer[0]='M';
+    	toWhom.getChars(0, toWhom.length(), Buffer, 1);
+    	if(context.length()>=150)
+    		return null;
+    	context.getChars(0, context.length(), Buffer, 30);
+    	return Buffer;
+    }
+    
 }
 
-
-//界面类
+//server的图形界面先放弃了！from 章昕
 class MyPanel extends JFrame implements Runnable,ActionListener
 {
     
     //定义参数
     String sender,getter;
-    MyBaseServer mbs = new MyBaseServer();
+    //MyBaseServer mbs = new MyBaseServer();
     //定义组件
     JPanel jp;
     JTextArea jta;
@@ -238,12 +170,12 @@ public void run()
         {
             continue;
         }
-        if(mbs.get_ino != null)
+        /*if(mbs.get_ino != null)
         {
             String s_board = this.jta.getText() + getter;        //通过面板来获取记录
             this.jta.setText(s_board);
             mbs.get_ino = null;
-        }
+        }*/
         this.repaint();    
     }
 }
@@ -253,7 +185,7 @@ public void run()
         if(e.getActionCommand() == "send")
         {
             //发送信息
-            mbs.send_ino = sender;        //更新消息缓存
+            //mbs.send_ino = sender;        //更新消息缓存
             String nwmes="You :" +sender +"\n";
             int len=nwmes.length();
             for(int i=0;i<80-len/4;i++) {
@@ -266,3 +198,25 @@ public void run()
         }
   }
 }
+
+public class server {
+   public static void main(String[] args) {
+	   List<myServerThread> servList=new ArrayList<myServerThread>();
+	   try{
+	   ServerSocket listen=new ServerSocket(9000);
+	   for(int i=0;i<20;i++)
+	   {
+		   myServerThread serv=new myServerThread(i);
+		   servList.add(serv);
+		   serv.run();
+		   //创建20个空的server等待连接
+	   }
+	   }catch(Exception ioe)
+	   {
+		   System.out.println(ioe.getMessage());
+	   }
+	   
+   }
+}
+
+
